f6d8a9c7d60bd8df0439c861c8e084744bacf366
Nicholas H.R. Sims <nickhrsims@gmail.com>
2023-02-16 14:44:26 -0700

    refactor: slow fold of pages into app

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,20 +1,29 @@
-import { Routes, Route, BrowserRouter } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Providers from './providers';
 import { DummyPage } from 'pages/dummy';
+import AuthPage from 'pages/auth';
 
 import { Scaffold } from './Scaffold';
 
 import { links } from './navigation';
 
+import { analystModel } from 'entities/analyst';
+
 export default function App() {
+    const auth = analystModel.useAuth();
+
     return (
         <Providers>
-            <Routes>
-                <Route path="/" element={<Scaffold links={links} title="Naive" />}>
-                    <Route index element={<DummyPage />} />
-                    <Route path={links.map.path} element={<DummyPage />} />
-                </Route>
-            </Routes>
+            {auth.isAuthenticated() ? (
+                <Routes>
+                    <Route path="/" element={<Scaffold links={links} title="Naive" />}>
+                        <Route index element={<DummyPage />} />
+                        <Route path={links.map.path} element={<DummyPage />} />
+                    </Route>
+                </Routes>
+            ) : (
+                <AuthPage />
+            )}
         </Providers>
     );
 }
