0249eae732740de13413618d76ca55e272c1038f
Nicholas H.R. Sims <nickhrsims@gmail.com>
2023-02-16 09:51:03 -0700

    refactor: app with dummy pages

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,10 +1,20 @@
+import { Routes, Route, BrowserRouter } from 'react-router-dom';
 import Providers from './providers';
 import { DummyPage } from 'pages/dummy';
+
+import { Scaffold } from './Scaffold';
+
+import { links } from './navigation';
 
 export default function App() {
     return (
         <Providers>
-            <DummyPage />
+            <Routes>
+                <Route path="/" element={<Scaffold links={links} title="Naive" />}>
+                    <Route index element={<DummyPage />} />
+                    <Route path={links.map.path} element={<DummyPage />} />
+                </Route>
+            </Routes>
         </Providers>
     );
 }
